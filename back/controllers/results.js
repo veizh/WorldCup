d1aa66a5b4ec1cd1e3cad9c72c288d01fdfa355c
Durville maxime <maximedurville95300@gmail.com>
2022-10-14 03:30:08 +0200

    ajout

--- back/controllers/results.js
+++ back/controllers/results.js
@@ -6,9 +6,8 @@ exports.createResult = async (req,res)=>{
     newResult.save()
     let players = await userSchema.find()
     
-    const result =  await req.body
+    const result =  await req.body.idMatch
     console.log(result);
-    console.log(req.body);
     players.map( (e)=>{
       
        console.log("user :" +e._id);
@@ -16,23 +15,26 @@ exports.createResult = async (req,res)=>{
        console.log( e.pari_test[result.idMatch]);
        console.log(e.pari_test[result.idMatch]===result.result);
        // si l'id du pari existe dans une des tables et que le resultat correspond a celui du user => ajouter des points 
-      if((e.pari_test && e.pari_test[result.idMatch] && e.pari_test[result.idMatch]===result.result) ||
-         (e.pari_a && e.pari_a[result.idMatch] && e.pari_a[result.idMatch]===result.result) ||
-         (e.pari_b && e.pari_b[result.idMatch] && e.pari_b[result.idMatch]===result.result) ||
-         (e.pari_c && e.pari_c[result.idMatch] && e.pari_c[result.idMatch]===result.result) ||
-         (e.pari_d && e.pari_d[result.idMatch] && e.pari_d[result.idMatch]===result.result) ||
-         (e.pari_e && e.pari_e[result.idMatch] && e.pari_e[result.idMatch]===result.result) ||
-         (e.pari_f && e.pari_f[result.idMatch] && e.pari_f[result.idMatch]===result.result) ||
-         (e.pari_g && e.pari_g[result.idMatch] && e.pari_g[result.idMatch]===result.result) ||
-         (e.pari_h && e.pari_h[result.idMatch] && e.pari_h[result.idMatch]===result.result)  ){
-      console.log('Gagné');
-      const newPoint =parseInt(e.point) + parseInt(result.point)
-       userSchema.updateOne({_id:e._id},{$set:{point:newPoint}})
-      console.log(e.point - parseInt(result.point) + " ~ to ~ " + e.point)
-    }
-    else {
-      console.log('Perdu');
-    }
+   //  if((e.pari_test && e.pari_test[result.idMatch] && e.pari_test[result.idMatch]===result.result) ||
+   //     (e.pari_a && e.pari_a[result.idMatch] && e.pari_a[result.idMatch]===result.result) ||
+   //     (e.pari_b && e.pari_b[result.idMatch] && e.pari_b[result.idMatch]===result.result) ||
+   //     (e.pari_c && e.pari_c[result.idMatch] && e.pari_c[result.idMatch]===result.result) ||
+   //     (e.pari_d && e.pari_d[result.idMatch] && e.pari_d[result.idMatch]===result.result) ||
+   //     (e.pari_e && e.pari_e[result.idMatch] && e.pari_e[result.idMatch]===result.result) ||
+   //     (e.pari_f && e.pari_f[result.idMatch] && e.pari_f[result.idMatch]===result.result) ||
+   //     (e.pari_g && e.pari_g[result.idMatch] && e.pari_g[result.idMatch]===result.result) ||
+   //     (e.pari_h && e.pari_h[result.idMatch] && e.pari_h[result.idMatch]===result.result)  ){
+   //  console.log('Gagné');
+   //  const newPoint =parseInt(e.point) + parseInt(result.point)
+   //   userSchema.updateOne({_id:e._id},{$set:{point:newPoint}})
+   //  console.log(e.point - parseInt(result.point) + " ~ to ~ " + e.point)
+   //}
+   //else {
+   //  console.log('Perdu');
+   //})
+     if(e.pari_a[result.idMatch] ){
+    console.log(e.pari_a[result.idMatch]);
+   }
     })
     return res.status(200).json({msg:"la mise a jour des points est fini"})
 
