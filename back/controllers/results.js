1a211d52f5e254a3f2e2ddae3e5dd748159373e7
Durville maxime <maximedurville95300@gmail.com>
2022-10-18 04:20:04 +0200

    ajout

--- back/controllers/results.js
+++ back/controllers/results.js
@@ -5,24 +5,24 @@ exports.createResult = async (req,res)=>{
     const newResult = new ResultSchema({...req.body})
     newResult.save()
     let players = await userSchema.find()
-    
+    const tmp = result.idMatch
     const result =  await req.body
   await  players.map( async (e)=>{
        
         console.log("user : " +e._id);
         console.log(result);
-        console.log(e.pari_a[result.idMatch]);
+        console.log(e.pari_a[tmp]);
 
        // si l'id du pari existe dans une des tables et que le resultat correspond a celui du user => ajouter des points 
-      if((e.pari_test && e.pari_test[result.idMatch] && e.pari_test[result.idMatch]===result.result) ||
-         (e.pari_a!==null && e.pari_a[result.idMatch] && e.pari_a[result.idMatch]===result.result) ||
-         (e.pari_b!==null && e.pari_b[result.idMatch] && e.pari_b[result.idMatch]===result.result) ||
-         (e.pari_c!==null && e.pari_c[result.idMatch] && e.pari_c[result.idMatch]===result.result) ||
-         (e.pari_d!==null && e.pari_d[result.idMatch] && e.pari_d[result.idMatch]===result.result) ||
-         (e.pari_e!==null && e.pari_e[result.idMatch] && e.pari_e[result.idMatch]===result.result) ||
-         (e.pari_f!==null && e.pari_f[result.idMatch] && e.pari_f[result.idMatch]===result.result) ||
-         (e.pari_g!==null && e.pari_g[result.idMatch] && e.pari_g[result.idMatch]===result.result) ||
-         (e.pari_h!==null && e.pari_h[result.idMatch] && e.pari_h[result.idMatch]===result.result)  ){
+      if((e.pari_test && e.pari_test[tmp] && e.pari_test[tmp]===result.result) ||
+         (e.pari_a!==null && e.pari_a[tmp] && e.pari_a[tmp]===result.result) ||
+         (e.pari_b!==null && e.pari_b[tmp] && e.pari_b[tmp]===result.result) ||
+         (e.pari_c!==null && e.pari_c[tmp] && e.pari_c[tmp]===result.result) ||
+         (e.pari_d!==null && e.pari_d[tmp] && e.pari_d[tmp]===result.result) ||
+         (e.pari_e!==null && e.pari_e[tmp] && e.pari_e[tmp]===result.result) ||
+         (e.pari_f!==null && e.pari_f[tmp] && e.pari_f[tmp]===result.result) ||
+         (e.pari_g!==null && e.pari_g[tmp] && e.pari_g[tmp]===result.result) ||
+         (e.pari_h!==null && e.pari_h[tmp] && e.pari_h[tmp]===result.result)  ){
 
        console.log("cet user gagne : " + result.point +" points.");
 
