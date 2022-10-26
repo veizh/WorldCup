517275f0f777fbc7e74fbf8f1287e8894e951b21
Durville maxime <maximedurville95300@gmail.com>
2022-10-26 19:22:50 +0200

    ajout

--- world-cup/src/utils/servers.js
+++ world-cup/src/utils/servers.js
@@ -1,2 +1,2 @@
-//exports.server = "https://worldcupback.vercel.app"
-exports.server = "http://"+window.location.hostname+":3500"
+exports.server = "https://worldcupback.vercel.app"
+//exports.server = "http://"+window.location.hostname+":3500"
