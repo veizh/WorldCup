38a45758f703177a377d709245678d465f4db725
Veizh <maximedurville95300@gmail.com>
2022-12-17 11:21:00 +0100

    testé

--- back/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-
-var commentShema = mongoose.Schema({
-    pseudo: { type: String, required: true, },
-    content:{ type: String, required: true }
-})
-
-module.exports = mongoose.model("comment",commentShema)
--- back/routes/comment.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-const commentCtrl = require('../controllers/comment');
-var router = express.Router();
-
-/* GET equipe listing. */
-router.post('/create', commentCtrl.createComment);
-router.get('/getComments',commentCtrl.getComments)
-module.exports = router;
