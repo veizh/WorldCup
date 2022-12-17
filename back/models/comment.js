147ac3b78d619d75d09cf0eb37edfb18a226b2b9
Veizh <maximedurville95300@gmail.com>
2022-12-17 11:53:18 +0100

    ok

--- back/models/comment.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-
-var commentSchema = mongoose.Schema({
-    pseudo: { type: String, required: true, },
-    content:{ type: String, required: true }
-})
-
-module.exports = mongoose.model("comment",commentSchema)
